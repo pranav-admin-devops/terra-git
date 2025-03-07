a78290b53a8458b2892068643654aa8ee2131de3
pranav <pranav@blah.com>
2025-03-07 09:26:09 +0530

    Added Public IP and Network Interface

--- resource.tf
+++ resource.tf
@@ -17,3 +17,24 @@ resource "azurerm_subnet" "sub" {
   virtual_network_name = azurerm_virtual_network.vent.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "my-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
