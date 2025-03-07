79d33204047c9edcd22d195a4714f407bd76740b
pranav <pranav@blah.com>
2025-03-07 09:28:22 +0530

    Added Network Securiy Group

--- resource.tf
+++ resource.tf
@@ -38,3 +38,48 @@ resource "azurerm_network_interface" "linux-nic" {
     public_ip_address_id = azurerm_public_ip.pip.id
   }
 }
+
+resource "azurerm_network_security_group" "sg" {
+  name                = "linux-sg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    security_rule {
+    name                       = "allow-http"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+    security_rule {
+    name                       = "allow-internet"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.linux-nic.id
+  network_security_group_id = azurerm_network_security_group.sg.id
+}
