af823d3690576057211aeb2f3ab025ba05d734e4
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-03-07 15:41:24 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -2,3 +2,39 @@ resource "azurerm_resource_group" "rg" {
   name     = "pranav-demo"
   location = "Central India"
 }
+
+resource "azurerm_virtual_network" "vent" {
+  name                = "pranav-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
+}
+
+resource "azurerm_subnet" "sub" {
+  name                 = "pranav-subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vent.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+resource "azurerm_public_ip" "pip" {
+  name                = "my-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
