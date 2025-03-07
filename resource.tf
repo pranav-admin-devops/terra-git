c89ed03c416d3ec107fbe93d312b5b6933170781
pranav <pranav@blah.com>
2025-03-07 09:30:42 +0530

    Added Linux Virtual Machine

--- resource.tf
+++ resource.tf
@@ -83,3 +83,29 @@ resource "azurerm_network_interface_security_group_association" "association" {
   network_interface_id      = azurerm_network_interface.linux-nic.id
   network_security_group_id = azurerm_network_security_group.sg.id
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "pranav-linux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "pranav"
+  admin_password = "Ubuntu@123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.linux-nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"  
+  }
+
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
