eaaface5392f8e77968122c36594e5cef9d520a9
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-03-07 14:05:40 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,111 +1,24 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "pranav-demo"
-  location = "Central India"
-}
-
-resource "azurerm_virtual_network" "vent" {
-  name                = "pranav-network"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
-}
-
-resource "azurerm_subnet" "sub" {
-  name                 = "pranav-subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vent.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-
-resource "azurerm_public_ip" "pip" {
-  name                = "my-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_interface" "linux-nic" {
-  name                = "linux-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.sub.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.pip.id
-  }
-}
-
-resource "azurerm_network_security_group" "sg" {
-  name                = "linux-sg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-    security_rule {
-    name                       = "allow-http"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-    security_rule {
-    name                       = "allow-internet"
-    priority                   = 101
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "association" {
-  network_interface_id      = azurerm_network_interface.linux-nic.id
-  network_security_group_id = azurerm_network_security_group.sg.id
-}
-
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "pranav-linux"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
-  admin_username      = "pranav"
-  admin_password = "Ubuntu@123"
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.linux-nic.id,
-  ]
+#   name                = "pranav-linux"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   size                = "Standard_F2"
+#   admin_username      = "pranav"
+#   admin_password = "Ubuntu@123"
+#   disable_password_authentication = false
+#   network_interface_ids = [
+#     azurerm_network_interface.linux-nic.id,
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"  
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"  
+#   }
 
-  source_image_reference {
-    publisher = "canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
-
+#   source_image_reference {
+#     publisher = "canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+# }
